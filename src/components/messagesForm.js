dce1165fa59971e72f96653850495df19e309020
arielAayala <ariel170802@gmail.com>
2022-09-23 02:38:15 -0300

    chat

--- src/components/messagesForm.js
+++ src/components/messagesForm.js
@@ -1,10 +1,18 @@
-export default function MessageForm(){
+import Attachment from "../assets/static/attachment"
+
+export default function MessageForm({handleSubmit,text,setText}){
 
 
 
     return (
-        <form>
-            <label htmlFor="img" ></label>
+        <form className="message_form" onSubmit={handleSubmit}>
+            <input type={"file"} id ="img" accept="image/" style={{display:"none"}} />
+            <div>
+                <input type={"text"} placeholder="Escribi su mensaje" value={text} onChange={e =>setText(e.target.value)} />
+            </div>
+            <div>
+                <button  className="btn">Enviar</button>
+            </div>
         </form>
     )
 }
--- src/conteiners/home.js
+++ src/conteiners/home.js
@@ -1,22 +1,28 @@
 
 import Header from "../components/header"
 import Footer from "../components/footer"
-import{collection,query,onSnapshot,where} from "firebase/firestore"
+import{collection,query,onSnapshot,where,addDoc, Timestamp,orderBy} from "firebase/firestore"
 import { db } from "../services/firebase"
 import { useState, useEffect } from "react"
 import { useAuth } from "../context/authContext"
 import User from "../components/User"
 import "../assets/media/app.css"
+import MessageForm from "../components/messagesForm"
+import Message from "../components/message"
 
 
 export default function Home(){
     const [users, setUsers] = useState([])
-    const{user}=useAuth()
+    const{user}= useAuth()
     const [chat, setChat] = useState("");
+    const [text, setText] = useState("");
+    const [msgs, setMsgs] = useState([]);
 
-    const selectUser = (user) =>{
-        setChat(user)
-    } 
+
+    const user1 = user.uid
+    const user2 = chat.uid
+    const idChat = user1 > user2 ? `${user1 + user2}` : `${user2+user1}`
+
 
     useEffect(() => {
         const userRef = collection(db,"usuarios")
@@ -31,7 +37,35 @@ export default function Home(){
         return () => onsub() 
     }, [])
 
-    
+    const selectUser = (user) =>{
+        setChat(user)
+
+        const user2 = user.uid
+        const idChat = user1 > user2 ? `${user1 + user2}` : `${user2+user1}`
+        const msgsRef = collection(db,"messages",idChat,"chat")
+        const q = query(msgsRef,orderBy("createdAt","asc"))
+        onSnapshot(q,querySnapshot =>{
+            let msgs = []
+            querySnapshot.forEach(doc =>{
+                msgs.push(doc.data())
+            })
+        setMsgs(msgs)
+        })
+    } 
+
+
+    const handleSumbit = async(e) =>{
+        e.preventDefault()
+        console.log(user1,user2)
+        await addDoc(collection(db,"messages",idChat,"chat"),{
+            text,
+            from:user1,
+            to:user2,
+            createdAt: Timestamp.fromDate(new Date())},
+        setText(""))
+    }
+
+
 
     return(
 
@@ -42,9 +76,20 @@ export default function Home(){
                     {users.map((user) => <User key={user.uid} user={user} selectUser={selectUser}/>)}
                 </div>
                 <div className="messages_container">
-                    {chat ? <div className="messages_user">
-                            <h3>{chat.displayName || chat.email}</h3>
-                        </div>: <h3 className="no_conv">Seleccionar chat</h3>}
+                    {chat ?
+                    (
+                    <>
+                    <div className="messages_user">
+                        <h3>{chat.displayName || chat.email}</h3>
+                    </div>
+                    <div className="messages">
+                        {msgs.length ? 
+                        msgs.map((msg,i)=> <Message key={i} msg={msg} user1={user1}/>):null}
+                    </div>
+                    <MessageForm handleSubmit={handleSumbit} text={text} setText={setText} />
+                    </>)
+                    : 
+                    (<h3 className="no_conv">Seleccionar chat</h3>)}
                 </div>
             </div>
             <Footer></Footer>
